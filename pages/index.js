63f2cffc9d21a3395850b28aeccbe33eb7a63431
Matthew Barnden <matt@aheadmedia.co.uk>
2017-09-21 19:54:02 +0100

    tidy up

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ import AuthenticatedUser from '../lib/getAuthenticatedUser'
 const Index = (props) => (
 
   <Layout {...props}>
-          <div className="bgWrap">
-						<h2>Welcome</h2>
-						<p>This site is an example of using Facebook login with an API</p>
-						<p>It uses React.js and Node.JS</p>
-						<p>There will also be a React Native app for both iOS and Android to follow</p>
-						<h3>On Login</h3>
-						<p>On login you will be able to see a list of dummy users and users who have also logged in to the app</p>
-					</div>
+    <div className="bgWrap">
+    <h2>Welcome</h2>
+      <p>This site is an example of using Facebook login with an API</p>
+      <p>It uses React.js and Node.JS</p>
+      <p>There will also be a React Native app for both iOS and Android to follow</p>
+      <h3>On Login</h3>
+      <p>On login you will be able to see a list of dummy users and users who have also logged in to the app</p>
+    </div>
   </Layout>
 )
 
