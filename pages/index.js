fc3a37d4b8093c8777d868a6dbd6b5804659fcb5
Matthew Barnden <matt@aheadmedia.co.uk>
2017-09-21 21:48:24 +0100

    new text for landing page

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,22 @@ import AuthenticatedUser from '../lib/getAuthenticatedUser'
 const Index = (props) => (
 
   <Layout {...props}>
-    <div className="bgWrap">
-    <h2>Welcome</h2>
-      <p>This site is an example of using Facebook login with an API</p>
-      <p>It uses React.js and Node.JS</p>
-      <p>There will also be a React Native app for both iOS and Android to follow</p>
+    <div class="bgWrap">
+      <h2>Welcome</h2>
+      <p>This site is an example of using Facebook login with an API using <a href="https://en.wikipedia.org/wiki/JSON_Web_Token">JSON web tokens</a></p>
+      <p>The front end is built using Next.JS, the code is available 
+      <a href="https://github.com/esharmony/auth-nextjs-web-app">here</a> and the backend API is build with Node.JS and this code can be found <a href="https://github.com/esharmony/authentication-api">here </a></p>
+      <p>There will also be a React Native app for both iOS and Android to follow and a React only web app.</p>
       <h3>On Login</h3>
-      <p>On login you will be able to see a list of dummy users and users who have also logged in to the app</p>
+      <p>On login you will be able to see a list of users who have also logged in to the app - thats it.</p>
+      <h3>Why?</h3>
+      <p>This app serves two main purposes for me. </p>
+      <ol>
+        <li>To learn</li>
+        <li>To be the base of apps I intend to build in the future needing a userbase.</li>
+      </ol>
+      <h3>Don't worry</h3>
+      <p>I am in no way interested in your email which is required for this app via the Facebook login, on login you will be able to delete yourself from this app and you will be completely removed from the DB.</p>
     </div>
   </Layout>
 )
