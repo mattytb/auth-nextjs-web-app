45cead7819931cacd4d1905ec6670ae8d145a7d3
Matthew Barnden <matt@aheadmedia.co.uk>
2017-09-24 16:47:13 +0100

    added settings

---
+++ settings.js
@@ -0,0 +1,7 @@
+export default {
+        baseUri:'https://shielded-hollows-37606.herokuapp.com',
+        authenticationStatusUri:'api/authenticationStatus',
+        fbAppId:302201110255626,
+        getFacebookUserUri:'api/authenticate-facebook',
+        usersUri:'api/users',
+}
