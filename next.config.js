1ec9b5886d5bd743c95c7dff567ba301c029f7a8
Matthew Barnden <matt@aheadmedia.co.uk>
2017-09-26 23:31:59 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-    distDir: 'build',
-    exportPathMap: () => {
-      return {
-        "/": { page: "/" },
-      }
-    },
     webpack: function (c) {
       if (c.resolve.alias) {
         delete c.resolve.alias['react']
         delete c.resolve.alias['react-dom']
       }
       return c
-    },
+    }
   }
